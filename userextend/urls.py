0da369c35901b33a53d54994cfb93cf5bd04f95b
Nata <ursutioan009@gmail.com>
2023-05-28 22:34:30 +0300

    26 commit/email send

--- userextend/urls.py
+++ userextend/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from userextend import views
+from userextend.views import error_view
 
 urlpatterns = [
     path('create-user/', views.UserExtendCreateView.as_view(), name='create_user'),
+    path('activate-user/<int:pk>/<str:token>/', views.activate_user, name='activate-user'),
+    path('error/', error_view, name='error'),
 ]
