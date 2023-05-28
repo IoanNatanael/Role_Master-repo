9781519df1e207ccbf385400bdf4c04f1e5e5b46
Nata <ursutioan009@gmail.com>
2023-05-28 13:08:20 +0300

    25 commit/discord bot invite page

--- bot_invite/views.py
+++ bot_invite/views.py
@@ -7,10 +7,7 @@ from django.contrib import messages
 @login_required
 @csrf_exempt
 def discord_link(request):
-    bot_link = 'https://discord.com/api/oauth2/authorize?client_id=1095629386050449449&permissions=1394522273792&' \
-               'redirect_uri=https%3A%2F%2Frole-master-2.herokuapp.com&response_type=code&scope=bot%20messages.' \
-               'read%20guilds.join%20gdm.join%20guilds%20guilds.members.read%20role_connections.write%20dm_channels.' \
-               'read%20relationships.read%20applications.commands.permissions.update'
+    bot_link = 'https://discord.com/api/oauth2/authorize?client_id=1095629386050449449&permissions=1394522273792&redirect_uri=https%3A%2F%2Frole-master-2.herokuapp.com&response_type=code&scope=bot%20messages.read%20guilds.join%20gdm.join%20guilds%20guilds.members.read%20role_connections.write%20dm_channels.read%20relationships.read%20applications.commands.permissions.update'
 
     if request.method == 'POST':
         # Copy button clicked, show success message
